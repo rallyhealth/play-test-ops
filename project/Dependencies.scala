b83e57b084e10c4b3cdb7dafc934bbc4f150780a
Jeff May <jeff.n.may@gmail.com>
2020-03-31 14:19:25 -0700

    Add support for Scala 2.11 for Play 2.7 to help upgrade path (#11)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,11 +4,11 @@ object Dependencies {
 
   final val Scala_2_11 = "2.11.12"
   final val Scala_2_12 = "2.12.6"
-  final val Scala_2_13 = "2.13.0"
+  final val Scala_2_13 = "2.13.1"
 
   final val Play_2_5 = "2.5.19"
   final val Play_2_6 = "2.6.19"
-  final val Play_2_7 = "2.7.3"
+  final val Play_2_7 = "2.7.4"
 
   def playServer(includePlayVersion: String): ModuleID = {
     "com.typesafe.play" %% "play" % includePlayVersion
@@ -19,6 +19,6 @@ object Dependencies {
   }
 
   val scalaTest: ModuleID = {
-    "org.scalatest" %% "scalatest" % "3.0.8"
+    "org.scalatest" %% "scalatest" % "3.1.1"
   }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 resolvers += Resolver.bintrayIvyRepo("rallyhealth", "sbt-plugins")
 
-addSbtPlugin("com.rallyhealth.sbt" %% "sbt-git-versioning" % "1.2.0")
+addSbtPlugin("com.rallyhealth.sbt" %% "sbt-git-versioning" % "1.4.0")
 addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.4")
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
